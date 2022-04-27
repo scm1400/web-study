1d3a02ff5f67a8c455240993bef1bd1db1616cec
Muice <scm1400@gmail.com>
2022-04-27 20:56:19 +0900

    코틀린 패키지와 임포트

---
+++ KotlinVerse/src/main/kotlin/BasicGrammer/7_Package&Import.kt
@@ -0,0 +1,6 @@
+package BasicGrammer
+
+//import foo.readInt as fooReadInt
+//import bar.readInt as barReadInt // as를 이용해서 별명을 붙여줄 수 있다
+
+// 자바에서는 파일의 현재 위치와 패키지명이 일치해야했지만, 코틀린은 그렇지 않아도된다, 하지만 추천하진 않는다
