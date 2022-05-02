d58aeb1c23c629335ae213821dfcc4e05fe494f8
Muice <scm1400@gmail.com>
2022-05-02 23:31:57 +0900

    코틀린 예외처리 내용 추가

--- KotlinVerse/src/main/kotlin/BasicGrammer/10_ExceptionHandling.kt
+++ KotlinVerse/src/main/kotlin/BasicGrammer/10_ExceptionHandling.kt
@@ -18,11 +18,40 @@ fun main() {
     }
 
     // try-catch 문
+    // 자바에서는 catch (FooException | BarException e){} 와 같이 한 캐치 블록 안에서 여러 예외를 처리할 수 있다.
+    // 코틀린에서는 아직 이런 핸들러를 지원하지 않는다
+
     fun readInt(default: Int): Int {
         try {
             return readLine()!!.toInt()
-        } catch (e: NumberFormatException) {
+        }
+        // 상위 예외를 먼저 작성하면 하위 예외로 도달 할 수 없기 때문에 하위 예외는 죽은코드이다
+        catch (e: Exception){
+            return 0
+        }
+        catch (e: NumberFormatException) {
             return default
         }
     }
+
+    // 자바와 달리 코틀린에서는 try 문이 식으로 쓰일수 있다.
+    fun readInt2(default: Int) = try {
+        readLine()!!.toInt()
+    } catch (e: NumberFormatException) {
+        default
+    }
+
+    // 자바와 달리 코틀링네서는 checked exception과 unchecked exception을 구분하지 않는다.
+
+
+    // finally 를 사용할 수 있다.
+    fun readInt3(default: Int) = try {
+        readLine()!!.toInt()
+    } finally {
+        default // 이렇게 사용할 수 없음 : try를 식으로 사용 할 경우, finally 값은 전체 try 블록의 값에 영향을 미치지 못한다.
+        println("Error")
+    }
+
+
+
 }
